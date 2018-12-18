409804672331ef1b475a70e98b2a4bda02ec6b16
Milan Andric <mandric+git@gmail.com>
2018-12-18 14:13:11 -0200

    clean up react node build script
    Could not think of any reason for these semicolons, also added the exit
    on error option to the shell so it exits with failure if any command
    fails.

--- build-rnnodeapp.sh
+++ build-rnnodeapp.sh
@@ -1,46 +1,50 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "Setting up...";
-rm -rf ./rnnodeapp;
-mkdir -p ./rnnodeapp;
+set -o errexit
+
+echo "Setting up..."
+rm -rf ./rnnodeapp
+mkdir -p ./rnnodeapp
 if [ "$1" = "benchmark" ]; then
-  cp -r ./benchmark/rnnodeapp ./;
+  cp -r ./benchmark/rnnodeapp ./
 else
-  cp -r ./src/rnnodeapp ./;
+  cp -r ./src/rnnodeapp ./
 fi
 
-echo "Installing dependencies...";
-cd ./rnnodeapp && npm i && cd ..;
+echo "Installing dependencies..."
+cd ./rnnodeapp
+npm i
+cd ..
 
-echo "Minifying...";
+echo "Minifying..."
 $(npm bin)/noderify \
   --filter original-fs \
   --replace.leveldown=leveldown-android-prebuilt \
-  ./rnnodeapp/index.js > ./rnnodeapp/_index.js;
-rm ./rnnodeapp/index.js;
-mv ./rnnodeapp/_index.js ./rnnodeapp/index.js;
+  ./rnnodeapp/index.js > ./rnnodeapp/_index.js
+rm ./rnnodeapp/index.js
+mv ./rnnodeapp/_index.js ./rnnodeapp/index.js
 
-echo "Setting up folder for native bindings...";
-mv ./rnnodeapp/node_modules/leveldown-android-prebuilt/compiled ./rnnodeapp/compiled;
+echo "Setting up folder for native bindings..."
+mv ./rnnodeapp/node_modules/leveldown-android-prebuilt/compiled ./rnnodeapp/compiled
 
-echo "Cleaning up...";
+echo "Cleaning up..."
 declare -a keepThese=("hyperlog" "osm-p2p-db-benchmark" "mapeo-styles")
 for x in "${keepThese[@]}"
 do
   if [ -e "./rnnodeapp/node_modules/$x" ]; then
-    mv ./rnnodeapp/node_modules/$x ./rnnodeapp/$x;
+    mv ./rnnodeapp/node_modules/$x ./rnnodeapp/$x
   fi
 done
-rm -rf ./rnnodeapp/node_modules;
-mkdir -p ./rnnodeapp/node_modules;
+rm -rf ./rnnodeapp/node_modules
+mkdir -p ./rnnodeapp/node_modules
 for x in "${keepThese[@]}"
 do
   if [ -e "./rnnodeapp/$x" ]; then
-    mv ./rnnodeapp/$x ./rnnodeapp/node_modules/$x;
+    mv ./rnnodeapp/$x ./rnnodeapp/node_modules/$x
   fi
 done
 
-echo "Bundling into Android...";
+echo "Bundling into Android..."
 $(npm bin)/react-native-node insert ./rnnodeapp
 
 echo "Done."
